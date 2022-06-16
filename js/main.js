4a48b3020bba2c0206c68a2fda2749d401442546
ducthien <ducthienle177@gmail.com>
2022-06-17 00:54:28 +0900

    fix typing animation script

--- js/main.js
+++ js/main.js
@@ -1,20 +1,55 @@
-// js for typing animation 
-// sentence, element reference( call by ID ) and delay time 
-async function typeSentence(sentence, eleRef, delay = 100) {
-    eleRef = document.getElementById(eleRef)
-    const letters = sentence.split("");
-    let i = 0;
-    while(i < letters.length) {
-      await waitForMs(delay);
-      $(eleRef).append(letters[i]);
-      i++
-    }
-    return;
-  }
-  
-  
-  function waitForMs(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
-  }
+//js typing animation
+const textDisplay = document.getElementById('text-display');
+const phrases = ['LE DUC THIEN', 'I love to code.'];
+let i = 0;
+let j = 0 ;
+let currentPhrase = [];
+let isDeleting = false;
+let isEnd = false;
 
-typeSentence("LE DUC THIEN", "#sentence");
+function loop () {
+  isEnd = false
+  textDisplay.innerHTML = currentPhrase.join('')
+
+  if (i < phrases.length) {
+
+    if (!isDeleting && j <= phrases[i].length) {
+      currentPhrase.push(phrases[i][j])
+      j++
+      textDisplay.innerHTML = currentPhrase.join('')
+    }
+
+    if(isDeleting && j <= phrases[i].length) {
+      currentPhrase.pop(phrases[i][j])
+      j--
+      textDisplay.innerHTML = currentPhrase.join('')
+    }
+
+    if (j == phrases[i].length) {
+      isEnd = true
+      isDeleting = true
+    }
+
+    if (isDeleting && j === 0) {
+      currentPhrase = []
+      isDeleting = false
+      i++
+      if (i === phrases.length) {
+        i = 0
+      }
+    }
+  }
+  const spedUp = Math.random() * (80 -50) + 50
+  const normalSpeed = Math.random() * (300 -200) + 200
+  const time = isEnd ? 3000 : isDeleting ? spedUp : normalSpeed
+  setTimeout(loop, time)
+}
+
+loop()
+
+
+
+
+
+
+
